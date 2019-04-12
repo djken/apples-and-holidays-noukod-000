ab3f2797124b92a78902f5a370ce74d003e8d979
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2019-04-12 14:35:02 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,6 +18,9 @@ def second_supply_for_fourth_of_july(holiday_hash)
     }
   }
   # return the second element in the 4th of July array
+  holiday_hash.each do |season, season_name| 
+    
+  end
   holiday_hash[:summer][:fourth_of_july][1]
 end
 
