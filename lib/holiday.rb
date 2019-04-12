bec9206bac48ddda7539bf72dc2e54d81a240e1e
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2019-04-12 14:56:28 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,6 +65,7 @@ def all_supplies_in_holidays(holiday_hash)
        puts "  #{holiday.to_s.split("_").map do|i| 
        i.capitalize}.join(" "): #{item.join(", ")}"
      end
+   end
   end 
 end
 
