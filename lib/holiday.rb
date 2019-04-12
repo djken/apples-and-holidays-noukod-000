b54f0aba2a6b2b0aee593d17cdfb151b9cfaaa23
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2019-04-12 15:24:24 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -67,22 +67,11 @@ def all_supplies_in_holidays(holiday_hash)
   end 
 end
 
-# def all_holidays_with_bbq(holiday_hash)
-#   # return an array of holiday names (as symbols) where supply lists
-#   # include the string "BBQ"
-  
-#   holiday_hash.map do |season, holiday|
-#     holiday.map do |holiday, item|
-#       holiday if item.include?("BBQ")
-#     end 
-#   end.flatten.compact
-# end
-
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists	  # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"	  # include the string "BBQ"
 
-answer = []
+array_symbol = []
 holiday_hash.each do |season, data|
   data.each do |holiday, value|
     value.each do |supply|
